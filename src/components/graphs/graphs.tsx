1a6aa0d6aa5945fdca184af96312560b5a166eaf
Glorison <lai.glorison@gmail.com>
2021-10-03 00:04:21 +1000

    Realised vega lite demo graphs

--- src/components/graphs/graphs.tsx
+++ src/components/graphs/graphs.tsx
@@ -3,15 +3,36 @@ import vegaEmbedModule from "vega-embed";
 // import bar_chart from './bar_chart.json'
 
 const Graphs = () => {
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
-    console.log("blah");
+    vegaEmbedModule("#line_chart", require("./line_chart.json"))
+      .then(function (res) {
+        console.log(res);
+      })
+      .catch(console.log);
+
     vegaEmbedModule("#bar_chart", require("./bar_chart.json"))
       .then(function (res) {
         console.log(res);
       })
       .catch(console.log);
+
+    vegaEmbedModule("#chloropleth", require("./chloropleth.json"))
+      .then(function (res) {
+        console.log(res);
+      })
+      .catch(console.log);
   });
-  return <div id="bar_chart" />;
+
+  return (
+    <div className={"w-screen"}>
+      <div id="line_chart" />
+      <div id="bar_chart" className={"overflow-auto w-screen"} />
+      <br />
+      <div id="chloropleth" className={"left"} />;
+    </div>
+  );
 };
 
 export default Graphs;
